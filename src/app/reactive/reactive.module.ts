13ed630e70be792ae02bb87772aba26f553c6691
Abel Borit <abelboritguitton@gmail.com>
2024-01-19 13:54:40 -0500

    [FEAT] create dynamic-page.component using "ng g component reactive/pages/dynamicPage --inline-style --skip-selector --skip-tests"

--- src/app/reactive/reactive.module.ts
+++ src/app/reactive/reactive.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
 import { BasicPageComponent } from './pages/basic-page/basic-page.component';
+import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 
 
 @NgModule({
   declarations: [
-    BasicPageComponent
+    BasicPageComponent,
+    DynamicPageComponent
   ],
   imports: [
     CommonModule,
