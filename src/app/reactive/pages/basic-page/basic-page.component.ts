ddac1157b33ac7d50b7227552d36def73c78188d
Abel Borit <abelboritguitton@gmail.com>
2024-01-19 17:55:24 -0500

    [FEAT] reactive forms first steps - reset & nonNullable

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -6,11 +6,18 @@ import {
   Validators,
 } from '@angular/forms';
 
+/* este podría ser un producto que nos envía el backend y al cargar la primera vez este componente lo tendríamos que establecer en el formulario y por eso se hace uso del OnInit y ngOnInit */
+const fakeProduct = {
+  name: 'Memory SSD',
+  price: 2500,
+  inStorage: 50,
+};
+
 @Component({
   templateUrl: './basic-page.component.html',
   styles: [],
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
   /* para trabjar con formularios reactivos se puede hacer con el FormGroup o con el servicio FormBuilder que una ventaja es que nos ayuda un poco al momento de crear un formulario pero al final de cuentas es lo mismo trajarlo con el FormGroup o con el FormBuilder y ya es cuestión de gustos y preferencias */
 
   /* una consideración a tener en cuenta es que cuando se trabajen con formularios reactivos es necesario que cada elemento de nuestro formulario (inputs, etc...) tienen que estar adentro de un elemento HTML padre, es decir, si se colocan dos inputs los cuales hacen referencia a las propiedades de mi formulario reactivo, entonces estos dos inputs tienen que estar adentro de un componente padre como un div, form, etc... pero debe tener un elemento HTML padre que los envuelva donde en este caso usaremos una etiqueta <form></form> */
@@ -31,8 +38,13 @@ export class BasicPageComponent {
 
   /* FORMA 2: inyectando el servicio FormBuilder que al final es una sintaxis un poco más cómoda porque a la larga lo que hace el FormBuilder es traducirlo a la forma del FormGroup() */
   constructor(private formBuilder: FormBuilder) {}
+  ngOnInit(): void {
+    /* otro uso bien común del reset() es que cuando nosotros tenemos una carga (por ejemplo, queremos entrar a una página web) y viene por el url y tenemos que leer esa url porque esos valores son valores que tenemos que establecer en el formulario, nosotros haremos eso mediante un OnInit y ngOnInit y técnicamente en el ngOnInit es donde vamos a inicializar nuestro formulario */
+    this.myForm.reset(fakeProduct);
+  }
+
   /* se podría obviar colocar el tipo FormGroup porque al final lo va a inferir pero es buena práctica tipar lo más que se pueda */
-  public myForm: FormGroup = this.formBuilder.group({
+  public myForm: FormGroup = this.formBuilder.nonNullable.group({
     // name: [valor por defecto, validaciones síncronas, validaciones asíncronas],
     // name: ['', [], []],
     name: ['', [Validators.required, Validators.minLength(3)]],
@@ -44,6 +56,16 @@ export class BasicPageComponent {
     /* ya están las validaciones arriba pero igual se puede mandar el formulario, ahora hay que comprobarlas y cuando se cumplan todas entonces recién se podría enviar el formulario */
     if (this.myForm.invalid) return; // si el formulario es inválido entonces que no haga nada
     console.log(this.myForm.value);
+
+    /* el reset hace dos funciones:
+      1. Regresa el formulario a su valor original
+      2. Si le mandamos un argumento entonces automáticamente establece los campos cuyos nombres coincidan con las propiedades de mi formulario. No es necesario colocar todas las propiedades porque el reset automáticamente lo hará por nosotros pero lo dejará como null */
+
+    /* valores por defecto pero será todo null. Si se coloca nonNullable en this.formBuilder.group() entonces volverá a sus valores iniciales que se le hayan definido */
+    // this.myForm.reset();
+
+    /* resetear el formulario colocando los valores que se le está pasando como argumento */
+    this.myForm.reset({ name: 'Laptop MK-II', price: 5000, inStorage: 50 });
   }
 }
 
