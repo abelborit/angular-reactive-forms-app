d37e01c03a263e9f7133a0218df6aa265a49931b
Abel Borit <abelboritguitton@gmail.com>
2024-01-23 13:28:40 -0500

    [FEAT] validations reactive forms first steps - validators.service.ts

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -62,34 +62,46 @@ export class RegisterPageComponent {
     private emailValidatorService: EmailValidatorService
   ) {}
 
-  public myForm: FormGroup = this.formBuilder.nonNullable.group({
-    name: [
-      '',
-      [
-        Validators.required,
-        Validators.pattern(this.validatorsService.firstNameAndLastnamePattern),
+  public myForm: FormGroup = this.formBuilder.nonNullable.group(
+    {
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(
+            this.validatorsService.firstNameAndLastnamePattern
+          ),
+        ],
       ],
-    ],
-    /* la validación de una expresión regular es síncrona porque ya tenemos toda la data para comparar contra la expresión regular y no se coloca el Validators.email ya que es más permisible y por eso usaremos Validators.pattern(expresión_a_colocar) */
-    /* aquí también se están usando validaciones asíncronas pero primero se disparan las validaciones síncronas y luego cuando las validaciones síncronas estén validadas correctamente se disparan las validaciones asíncronas que se pueden colocar mediante el new para crear una nueva instancia o sino mendiante la inyección del servicio */
-    // email: ['', [Validators.required, Validators.email]],
-    email: [
-      '',
-      [
-        Validators.required,
-        Validators.pattern(this.validatorsService.emailPattern),
+      /* la validación de una expresión regular es síncrona porque ya tenemos toda la data para comparar contra la expresión regular y no se coloca el Validators.email ya que es más permisible y por eso usaremos Validators.pattern(expresión_a_colocar) */
+      /* aquí también se están usando validaciones asíncronas pero primero se disparan las validaciones síncronas y luego cuando las validaciones síncronas estén validadas correctamente se disparan las validaciones asíncronas que se pueden colocar mediante el new para crear una nueva instancia o sino mendiante la inyección del servicio */
+      // email: ['', [Validators.required, Validators.email]],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.validatorsService.emailPattern),
+        ],
+        // [new EmailValidatorService()], // validación asíncrona con el new EmailValidatorService() para crear una nueva instancia lo cual no es malo trabajar así pero si se tiene algún proceso en el email-validator.service.ts donde establece alguna conexión o se usa mucho y queremos reutilizar la misma instancia entonces es mejor inyectar el servicio para usarlo directamente las veces que sea necesario porque si hubieran muchas inicializaciones con el new EmailValidatorService() entonces afecta más al rendimiento VS usar el servicio this.emailValidatorService ya que al final es la misma instancia que se usará a lo largo de todas las validaciones (igual tendría que ser algo demasiado grande para que se vea afectado en el rendimiento pero es mejor ahorrarnos esa preocupación por eso se hará con el servicio)
+        [this.emailValidatorService], // inyectar el servicio y luego usarlo directamente y no usarlo como this.emailValidatorService.validate ya que la clase (el servicio) implementa "AsyncValidator" por lo tanto, el formulario ya sabe que es de este tipo y puede determinar cómo usar la validación. No es necesario llamar al método validate ya que lo hace de manera implícita
       ],
-      // [new EmailValidatorService()], // validación asíncrona con el new EmailValidatorService() para crear una nueva instancia lo cual no es malo trabajar así pero si se tiene algún proceso en el email-validator.service.ts donde establece alguna conexión o se usa mucho y queremos reutilizar la misma instancia entonces es mejor inyectar el servicio para usarlo directamente las veces que sea necesario porque si hubieran muchas inicializaciones con el new EmailValidatorService() entonces afecta más al rendimiento VS usar el servicio this.emailValidatorService ya que al final es la misma instancia que se usará a lo largo de todas las validaciones (igual tendría que ser algo demasiado grande para que se vea afectado en el rendimiento pero es mejor ahorrarnos esa preocupación por eso se hará con el servicio)
-      [this.emailValidatorService], // inyectar el servicio y luego usarlo directamente y no usarlo como this.emailValidatorService.validate ya que la clase (el servicio) implementa "AsyncValidator" por lo tanto, el formulario ya sabe que es de este tipo y puede determinar cómo usar la validación. No es necesario llamar al método validate ya que lo hace de manera implícita
-    ],
-    /* al crear validaciones personalizadas no se invoca la función, es decir, no se coloca this.validatorsService.cantBeSameUser(), solo se le pasa por referencia, es decir, this.validatorsService.cantBeSameUser, para que Angular sea quien la ejecute */
-    username: [
-      '',
-      [Validators.required, this.validatorsService.cantBeSameUser],
-    ],
-    password: ['', [Validators.required, Validators.minLength(6)]],
-    password2: ['', [Validators.required]],
-  });
+      /* al crear validaciones personalizadas no se invoca la función, es decir, no se coloca this.validatorsService.cantBeSameUser(), solo se le pasa por referencia, es decir, this.validatorsService.cantBeSameUser, para que Angular sea quien la ejecute */
+      username: [
+        '',
+        [Validators.required, this.validatorsService.cantBeSameUser],
+      ],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      password2: ['', [Validators.required]],
+    },
+    {
+      /* al hacerlo de esta forma tenemos la opción de colocar otro objeto el cual puede tener validadores asíncronos, validadores síncronos y condiciones para actualizar pero este segundo objeto será utilizado más para los validadores asíncronos o validadores síncronos pero de forma grupal, es decir, a nivel de todo el formulario como tal */
+      /* la ventaja de hacerlo así es que a las funciones que se le coloquen al array de validators se les va a pasar como argumento implícito todo el formulario con la finalidad de tener todo el formulario y sus campos, hacerlo como arriba es a nivel de control (por cada campo) y hacerlo aquí es a nivel de formulario en general */
+      validators: [
+        /* aquí solo se están mandando los nombre de los campos a evaluar, por eso esperaba field1 y field1 2 de tipo string */
+        this.validatorsService.isFieldOneEqualFieldTwo('password', 'password2'),
+      ],
+    }
+  );
 
   isNotValidField(field: string): boolean | null {
     // console.log('render isNotValidField');
--- src/app/shared/services/validators.service.ts
+++ src/app/shared/services/validators.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
-import { FormControl, ValidationErrors, FormGroup } from '@angular/forms';
+import {
+  FormControl,
+  ValidationErrors,
+  FormGroup,
+  AbstractControl,
+} from '@angular/forms';
 
 @Injectable({ providedIn: 'root' })
 export class ValidatorsService {
@@ -24,8 +29,67 @@ export class ValidatorsService {
   }
 
   /* tendrá como objetivo simplificar la forma en cómo generar la validación si el campo está correcto o incorrecto y estará revisando si el campo en su objeto errors tiene algún error y si el campo fue tocado lo cual al final el método isNotValidField() devolverá un true o un false o también puede ser un null ya que el objeto errors si no tiene nada es un null y el touched devuelve un boolean */
-  isNotValidField(form: FormGroup, field: string): boolean | null {
+  public isNotValidField(form: FormGroup, field: string): boolean | null {
     // console.log('render isNotValidField');
     return form.controls[field].errors && form.controls[field].touched;
   }
+
+  public isFieldOneEqualFieldTwo(field1: string, field2: string) {
+    /* en este método se están recibiendo dos parámetros a evaluar entonces se retornará una función la cual tendrá como argumento a mi formulario y me permite tener control sobre él (el formulario que se le está pasando implícitamente) que es de tipo AbstractControl (FormControl, FormGroup, FormArray) y me regresará ValidationErrors | null o si es para validaciones asíncronas sería Observable<ValidationErrors | null> */
+    /* La referencia al formGroup se obtiene de forma automática cuando esta función se utiliza como validación en un formulario. Angular gestiona internamente la inyección del formGroup, por lo que no es necesario pasar explícitamente el formGroup como parámetro. Cuando esta función se utiliza como validación en un formulario, Angular se encarga de proporcionar el formGroup asociado al formulario en curso, permitiendo así acceder a los valores de los campos y establecer errores según sea necesario. */
+    return (formGroup: AbstractControl): ValidationErrors | null => {
+      // console.log({ formGroup });
+
+      /* aquí toca leer los campos donde se harán la validación ya que solo estoy recibiendo string en isFieldOneEqualFieldTwo y entonces al ya tener los campos y esta función que tendrá el formulario como tal (todos los campos) ya se puede empezar con las validaciones o evaluaciones */
+      const fieldValue1 = formGroup.get(field1)?.value;
+      const fieldValue2 = formGroup.get(field2)?.value;
+      const field2ValidatorsErrors = formGroup.get(field2)?.errors;
+      // console.log(field2ValidatorsErrors);
+
+      if (fieldValue1 !== fieldValue2) {
+        /* aquí se tendría que establecer por ejemplo al field1 o field2 el error ya que no cumplirá la validación, en este caso se colocará dentro de los errores del field2 ya que es donde se repite la contraseña. En teoría hasta aquí sería más que suficiente porque un campo del formulario tener algún error entonces todo el formulario estará como INVALID */
+        formGroup
+          .get(field2)
+          ?.setErrors({ ...field2ValidatorsErrors, notEqualField: true });
+
+        /* este es el error que va a retornar en caso la validación no se cumpla. Esta respuesta va a los errores globales del formulario */
+        return { notEqualField: true };
+      }
+
+      /* FORMA 1: establecer todos los errores como null (su caso de uso sería cuando solo se tiene una validación) */
+      /* si todo sale bien y cumple la validación entonces hay que limpiar los errores del campo pero hay que tener cuidado ya que esto limpiará todos los errores de ese campo y se tendría que buscar solo el error colocado (si tiene validaciones como campo requerido, campo con mínimo de caracteres, etc, eso se queda ya que siempre lo tendrá verificando y ejecutando) */
+      // formGroup.get(field2)?.setErrors(null);
+      // return null;
+
+      /* FORMA 2: eliminar el error específico que es el notEqualField (su caso de uso sería cuando se tienen varias validaciones y solo se quiere eliminar una validación en específico) */
+      // if (formGroup.get(field2)?.hasError('notEqualField')) {
+      //   delete formGroup.get(field2)?.errors?.['notEqualField'];
+      //   /* cuando agrega o elimina un validador en tiempo de ejecución, debe llamar updateValueAndValidity() para que la nueva validación entre en vigor */
+      //   formGroup.get(field2)?.updateValueAndValidity();
+      // }
+      // /* este return únicamente se realiza si los dos campos son iguales para que pase la validación */
+      // return null;
+
+      /* FORMA 3: retornar solo el null para que no choque con las demás validaciones ya que con el return null; la funcion ya limpia la validación de la propia funcion */
+      return null;
+    };
+  }
 }
+
+/* ******************************************************************************************************************* */
+/* ¿Por qué en las validaciones enviamos una referencia a la función y no el llamado de la función como tal? */
+/* Esto es porque angular es quien debe ejecutar las validaciones cuando ocurran los cambios en los formularios por ello requiere la referencia a los metodos a ejecutar. Si se pasa como parámetro la ejecución de un metodo solo se ejecutaría apenas se monte el componente de angular pero no cuando ocurran cambios, ya que no se tiene la referencia sino el valor que este retorne */
+
+/* ******************************************************************************************************************* */
+/* ¿Por qué la función isFieldOneEqualFieldTwo debe retornar otra función? */
+/* Esto es porque se requiere de una función que se ejecute cada vez que cambien los valores de password y password2, lo que implica es que estamos retornando la referencia a dicha función anonima que valida si los campos son iguales.
+
+En los validators podemos tener un array de funciones validadoras ahora como necesitamos validar estos 2 campos, tener un retorno como true o false o seria algo valido por el tipado por retornamos una función.
+
+Ahora, ¿Por qué tiene que ser una función? cada vez que ocurran cambios en nuestro formulario debe volver a validarse, angular internamente ejecutará estas funciones y si se obtiene un error es porque la validación fallo. Sería algo similar a los listeners con vainilla javascript, ya que si tenemos un listener al evento change de un input a dicho listener le pasamos como segundo argumento un callback (la definicion de una funcion) el cual se ejecutara cada vez que ocurre un cambio.
+
+Entonces si la función isFieldOneEqualFieldTwo no retornara una función, ¿Angular no podria nunca validar estos 2 campos de password1 y password2? Correcto, ya que no tendria forma de ejecutar la función validadora que necesitamos para comprobar si algún campo (o campos como este caso) son validos.
+
+Pero, ¿Por qué en la funcion cantBeSameUser no es necesario que retorne una funcion?
+Porque cantBeSameUser ya es una función y no se está ejecutando sino que se está pasando por referencia, caso similar con el retorno de la función isFieldOneEqualFieldTwo, ese return es una referencia a la función que valida los campos, tal como se comenta arriba y isFieldOneEqualFieldTwo sí se está ejecutando porque se le está pasando parámetros adicionales y el return de isFieldOneEqualFieldTwo es quien recibe el formulario de forma implícita, también que las funciones de validación son funciones que se deben retornar a AbstractControl, por tal razón, necesitamos retornar una función de tipo ValidationErrors | null
+*/
